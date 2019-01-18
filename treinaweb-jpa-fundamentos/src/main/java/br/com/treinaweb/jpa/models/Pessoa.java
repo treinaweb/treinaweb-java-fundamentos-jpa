5a6fef6217de6dadb5a57836aaa1066b4a5c533e
Cleber Campomori <cleber.campomori@gmail.com>
2019-01-17 23:22:09 -0200

    Aula 4.2: Criando o primeiro Entity Bean

---
+++ treinaweb-jpa-fundamentos/src/main/java/br/com/treinaweb/jpa/models/Pessoa.java
@@ -0,0 +1,60 @@
+package br.com.treinaweb.jpa.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PES_PESSOAS")
+public class Pessoa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "PES_ID")
+	private int id;
+
+	@Column(name = "PES_NOME", nullable = false, length = 20)
+	private String nome;
+
+	@Column(name = "PES_SOBRENOME", nullable = false, length = 30)
+	private String sobrenome;
+
+	@Column(name = "PES_IDADE", nullable = false)
+	private String idade;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getIdade() {
+		return idade;
+	}
+
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+
+}
